9bc925591196b59e1b6ad97ee2b51e9a8e6f104c
g1azed <victoria1230120@gmail.com>
2024-11-04 04:16:30 +0900

    PostCard 수정

--- src/stickerbook/Sticker.jsx
+++ src/stickerbook/Sticker.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 
 const importHtml = () => {
-    return {__html: '<iframe src="https://g1azed.github.io/postCard/" style="width:100vw; height:100vw;"></iframe>'};
+    return {__html: '<iframe src="https://g1azed.github.io/postCard/" style="width:99vw; height:100vh;" frameborder="0"></iframe>'};
 }
 const Sticker = () => {
     return (
